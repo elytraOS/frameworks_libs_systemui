3f8d7ba7074f601f9af74bca2f9e9e993989f492
Amir Zaidi <azaidi@live.nl>
2020-01-11 16:57:26 +0100

    IconLoader: Add config hint to bypass adaptive icon wrapping
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: If072f69887de39eafaccdf6acfb7461f75d0bf17
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
+++ iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
@@ -36,6 +36,8 @@ import com.android.launcher3.icons.BitmapInfo.Extender;
  */
 public class BaseIconFactory implements AutoCloseable {
 
+    public static final int CONFIG_HINT_NO_WRAP = 0x1000000;
+
     private static final String TAG = "BaseIconFactory";
     private static final int DEFAULT_WRAPPER_BACKGROUND = Color.WHITE;
     static final boolean ATLEAST_OREO = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
@@ -303,7 +305,8 @@ public class BaseIconFactory implements AutoCloseable {
             dr.setBounds(0, 0, 1, 1);
             boolean[] outShape = new boolean[1];
             scale = getNormalizer().getScale(icon, outIconBounds, dr.getIconMask(), outShape);
-            if (!(icon instanceof AdaptiveIconDrawable) && !outShape[0]) {
+            if (!(icon instanceof AdaptiveIconDrawable) && !outShape[0]
+                    && (icon.getChangingConfigurations() & CONFIG_HINT_NO_WRAP) == 0) {
                 ThemedIconDrawable.ThemeData themeData = null;
                 if (icon instanceof ThemedIconDrawable.ThemedBitmapIcon) {
                     themeData = ((ThemedIconDrawable.ThemedBitmapIcon) icon).mThemeData;
